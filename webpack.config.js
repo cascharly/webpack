f7419cec212e45b85fb01ad9c715dfc00a760fff
cascharly <cascharly@gmail.com>
2021-01-23 12:59:59 -0300

    09 styles

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
         test: /\.html$/,
         loader: "html-loader",
         options: {
